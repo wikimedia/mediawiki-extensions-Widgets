0156ad6150dfc0b2a1dbb82169b8d59ed3667c8c
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-06-13 20:17:19 +0000

    Localisation updates for extension messages from translatewiki.net (2009-06-13 19:49 UTC)

--- Widgets.i18n.php
+++ Widgets.i18n.php
@@ -14,3 +14,49 @@ $messages['en'] = array(
 	'grouppage-widgeteditor' => 'Project:Widget editors',
 	'right-editwidgets' => 'Edit pages in the "Widget" namespace',
 );
+
+/** Message documentation (Message documentation)
+ * @author Fryed-peach
+ * @author Siebrand
+ */
+$messages['qqq'] = array(
+	'grouppage-widgeteditor' => '{{doc-important|Leave "Project:" untranslated}}',
+	'right-editwidgets' => '{{doc-right}}',
+);
+
+/** Belarusian (Taraškievica orthography) (Беларуская (тарашкевіца))
+ * @author Jim-by
+ */
+$messages['be-tarask'] = array(
+	'widgets-desc' => 'Дазволіць адміністратарам вікі дадаваць вольную форму кіраваньня акном ў вікі праз рэдагаваньне старонак у прасторы назваў Widget. Арыгінальная распрацоўка для [http://www.ardorado.com Ardorado.com]',
+	'widgets-error' => 'Памылка ў [[Widget:$1|кіраваньні акном $1]]',
+	'group-widgeteditor' => 'Рэдактар кіраваньня акном',
+	'group-widgeteditor-member' => 'Рэдактар кіраваньня акном',
+	'grouppage-widgeteditor' => 'Праект:Рэдактар кіраваньня акном',
+	'right-editwidgets' => 'Рэдагаваць старонкі ў прасторы назваў «Widget»',
+);
+
+/** Japanese (日本語)
+ * @author Fryed-peach
+ */
+$messages['ja'] = array(
+	'widgets-desc' => '管理者が「Widget」名前空間内のページを編集し、ウィキに自由な形式のウィジェットを追加できるようにする。元々は [http://www.ardorado.com Ardorado.com] のために開発されました',
+	'widgets-error' => '[[Widget:$1|ウィジェット $1]]にエラー',
+	'group-widgeteditor' => 'ウィジェット編集者',
+	'group-widgeteditor-member' => 'ウィジェット編集者',
+	'grouppage-widgeteditor' => 'Project:ウィジェット編集者',
+	'right-editwidgets' => '「Widget」名前空間のページを編集する',
+);
+
+/** Dutch (Nederlands)
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'widgets-desc' => "Stelt wikibeheerders in staat naar eigen inzicht widgets aan de wiki toe te voegen door het bewerken van pagina's in de naamruimte Widget. Oorspronkelijk ontwikkeld voor [http://www.ardorado.com Ardorado.com]",
+	'widgets-error' => 'Fout in [[Widget:$1|widget $1]]',
+	'group-widgeteditor' => 'Widgetbewerkers',
+	'group-widgeteditor-member' => 'Widgetbewerker',
+	'grouppage-widgeteditor' => 'Project:Widgetbewerkers',
+	'right-editwidgets' => 'Pagina\'s bewerken in de naamruimte "Widget"',
+);
+
