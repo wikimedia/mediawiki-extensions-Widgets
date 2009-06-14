f08cc4898a0cd384502acff3c873ec655bdd6b75
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-06-14 14:27:21 +0000

    Message tweaks

--- Widgets.i18n.php
+++ Widgets.i18n.php
@@ -7,12 +7,13 @@
 $messages = array();
 
 $messages['en'] = array(
-	'widgets-desc' => 'Allows wiki administrators to add free-form widgets to the wiki by editing pages within the Widget namespace. Originally developed for [http://www.ardorado.com Ardorado.com]',
-	'widgets-error' => 'Error in [[Widget:$1|widget $1]]',
+	'widgets-desc' => 'Allows wiki administrators to add free-form widgets to the wiki by editing pages within the {{ns:widget}} namespace.
+Originally developed for [http://www.ardorado.com Ardorado.com]',
+	'widgets-error' => 'Error in [[{{ns:widget}}:$1|widget $1]]',
 	'group-widgeteditor' => 'Widget editors',
 	'group-widgeteditor-member' => 'Widget editor',
 	'grouppage-widgeteditor' => 'Project:Widget editors',
-	'right-editwidgets' => 'Edit pages in the "Widget" namespace',
+	'right-editwidgets' => 'Edit pages in the "{{ns:widget}}" namespace',
 );
 
 /** Message documentation (Message documentation)
