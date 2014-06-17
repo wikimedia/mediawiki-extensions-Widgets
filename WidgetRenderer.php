98b075bda12a892d09cb7786d8c52609fd0d112d
JoelKP <joelkpettersson@gmail.com>
2014-06-18 01:13:19 +0200

    Replace deprecated wfMsgExt call.
    Change-Id: I51dd3d4c077840b2c11b708274fd6c4a233af236

--- WidgetRenderer.php
+++ WidgetRenderer.php
@@ -127,7 +127,7 @@ class WidgetRenderer {
 		try {
 			$output = $smarty->fetch( "wiki:$widgetName" );
 		} catch ( Exception $e ) {
-			return '<div class=\"error\">' . wfMsgExt( 'widgets-error', array( 'parsemag' ), htmlentities( $widgetName ) ) . '</div>';
+			return '<div class=\"error\">' . wfMessage( 'widgets-error', htmlentities( $widgetName ) )->text() . '</div>';
 		}
 
 		// Hide the widget from the parser.
