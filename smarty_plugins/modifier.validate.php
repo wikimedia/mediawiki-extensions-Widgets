ce45a19bf836e7ef010cc09ce4c0beea5fc6c72f
Brian Wolff <bawolff+wn@gmail.com>
2023-02-11 09:04:17 -0800

    Use better url validation. Block javascript: urls and allow protocol rel
    PHP's url validation works differently from how most people expect.
    It blocks protocol relative urls, but still lets dangerous things
    through like javascript://%0aalert(1) or
    https://example.com#"><script>alert(2)</script>. Replace it with
    validation based on MediaWiki. A significant portion of insecure
    widgets are due to misunderstanding what validate:url does. Instead
    of trying to ensure everyone understands what it actually does, it
    seems like it would be better just to make it do what people think
    it does.
    
    This is a bit of a breaking change, as this restricts valid url
    protocols to those configured in $wgUrlProtocols. Retain 'url-php'
    for the old behaviour in case anyone needs it.
    
    Bug: T329349
    Change-Id: I0d2786242195cc3331dd4139a1dd4d59e6c0f1b8
    (cherry picked from commit 86386af3d19f25d182dd3a8170e7e5910f55d1e7)

--- smarty_plugins/modifier.validate.php
+++ smarty_plugins/modifier.validate.php
@@ -10,9 +10,35 @@
  * -------------------------------------------------------------
  */
 function smarty_modifier_validate( $string, $type='url' ) {
+
+	// treat url validation differently. PHP validation doesn't allow
+	// protocol relative urls, well at the same time not blocking
+	// urls like javascript://%0aalert(1);//#"><script>alert(2)</script>
+
+	if ( $type === 'url' ) {
+		// Transform the url into an equivalent form without problematic
+		// characters. In theory people should sanitize separately. In
+		// practise they do not. Replacing these should not change the meaning
+		// of the url.
+		$string = str_replace( [ "'", '"', '<' ], [ '%27', '%22', '%3C' ], $string );
+		if ( substr( $string, -1 ) === "\\" ) {
+			// Last character \ can be problematic in JS contexts
+			// How to convert depends on if before or after the ?
+			$replacement = strpos( $string, '?' ) === false ? '/' : '%5C';
+			$string = substr( $string, 0, -1 ) . $replacement;
+		}
+
+		// Note, this matches protocol relative, but not relative urls
+		// and only allows whatever is in $wgUrlProtocols.
+		if ( preg_match( '/^(?i:' . wfUrlProtocols() . ')\S+$/', $string ) ) {
+			return $string;
+		}
+		return '';
+	}
+
 	// mapping for PHP filters (http://us2.php.net/manual/en/filter.constants.php)
 	$filters = array(
-		'url' => FILTER_VALIDATE_URL,
+		'url-php' => FILTER_VALIDATE_URL,
 		'int' => FILTER_VALIDATE_INT,
 		'boolean' => FILTER_VALIDATE_BOOLEAN,
 		'float' => FILTER_VALIDATE_FLOAT,
